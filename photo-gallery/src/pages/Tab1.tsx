3b86709af6c6e76575ad4053e8e46e872393e1bc
Aidan Braun <aidancbraun@gmail.com>
2023-12-12 03:33:53 +0000

    Week 17 commit

--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -1,22 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+  const [dataset, setDataset] = useState<any[]>([]);
+  
+  const dataURL = "https://dev-cs50c-week-11.pantheonsite.io/wp-json/twentytwentythree-child/v1/contacts"
+
+  useEffect(() => {
+    fetch(dataURL) // fetch() to load raw json string
+    .then(response => response.json()) // json() to convert raw string to json object
+
+    .then(data => setDataset(data)); // react state set function to populate data state var
+  },[])
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Contacts</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Contacts</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {/* use map() to loop through JSON array returned from WP */}
+        <IonList id="contact-list">
+          <IonListHeader>Contact List</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines="inset" key={index}>
+              <IonLabel>
+                <h4>{item.first_name}, {item.last_name}</h4>
+                <p>{item.phone_number}</p>
+                <p>{item.email}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- photo-gallery/src/pages/Tab3.tsx
+++ photo-gallery/src/pages/Tab3.tsx
@@ -1,22 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './Tab1.css';
 
 const Tab3: React.FC = () => {
+
+  const [dataset, setDataset] = useState<any[]>([]);
+
+  const dataURL = "https://dev-cs50c-week-11.pantheonsite.io/wp-json/twentytwentythree-child/v1/products"
+
+  useEffect(() => {
+    fetch(dataURL) // fetch() to load raw json string
+    .then(response => response.json()) // json() to convert raw string to json object
+
+    .then(data => setDataset(data)); // react state set function to populate data state var
+  },[])
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Products</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Products</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        {/* use map() to loop through JSON array returned from WP */}
+        <IonList id="contact-list">
+          <IonListHeader>Product List</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines="inset" key={index}>
+              <IonLabel>
+                <h4>{item.product_name}</h4>
+                <p>{item.product_price}</p>
+                <p>{item.product_description}</p>
+                <img src={item.guid}></img>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
